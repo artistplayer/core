006ae0e4a2d6f862829bba242f4b33e7f264fc20
Maikel ten Voorde <info@matevo.nl>
2019-03-20 14:22:23 +0100

    TTC

--- app/Console/Commands/Socket.php
+++ app/Console/Commands/Socket.php
@@ -2,41 +2,59 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
 
-class Socket extends Command
+class Socket extends \Illuminate\Console\Command implements \Ratchet\MessageComponentInterface
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'socket:create {port}';
+    protected $signature = 'socket';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create Websocket';
+    protected $description = 'Run Socket Server';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        //
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    $this
+                )
+            ),
+            9000
+        );
+        $server->run();
     }
+
+    function onOpen(\Ratchet\ConnectionInterface $conn)
+    {
+        // TODO: Implement onOpen() method.
+    }
+
+    function onClose(\Ratchet\ConnectionInterface $conn)
+    {
+        // TODO: Implement onClose() method.
+    }
+
+    function onError(\Ratchet\ConnectionInterface $conn, \Exception $e)
+    {
+        // TODO: Implement onError() method.
+    }
+
+    function onMessage(\Ratchet\ConnectionInterface $from, $msg)
+    {
+        // TODO: Implement onMessage() method.
+    }
+
+
 }
