12f77b03fa32c7390ab3009a9fd291a522ddeeab
Maikel ten Voorde <info@matevo.nl>
2019-03-22 11:02:46 +0100

    TTC

--- app/Console/Commands/Stats.php
+++ app/Console/Commands/Stats.php
@@ -38,27 +38,29 @@ class Stats extends \Illuminate\Console\Command
 
     static public function update()
     {
-        exec("bin/omxcontrols get position", $position);
-        exec("bin/omxcontrols get duration", $duration);
-        exec("bin/omxcontrols get volume", $volume);
-        exec("bin/omxcontrols get source", $source);
-        exec("bin/omxcontrols get status", $status);
+        @exec("bin/omxcontrols get position", $position);
+        @exec("bin/omxcontrols get duration", $duration);
+        @exec("bin/omxcontrols get volume", $volume);
+        @exec("bin/omxcontrols get source", $source);
+        @exec("bin/omxcontrols get status", $status);
 
 
-        $integrity_hash = explode("/media", join(PHP_EOL, $source));
-        $integrity_hash = explode("/", $integrity_hash[0]);
-        $integrity_hash = end($integrity_hash);
-        $file = File::all()->where('integrity_hash', '=', $integrity_hash)->first();
+        if ($source) {
+            $integrity_hash = explode("/media", join(PHP_EOL, $source));
+            $integrity_hash = explode("/", $integrity_hash[0]);
+            $integrity_hash = end($integrity_hash);
+            $file = File::all()->where('integrity_hash', '=', $integrity_hash)->first();
+        }
 
         return json_encode([
             'method' => 'playstate',
             'value' => [
-                'position' => join(PHP_EOL, $position),
-                'duration' => join(PHP_EOL, $duration),
-                'volume' => join(PHP_EOL, $volume),
-                'source' => join(PHP_EOL, $source),
-                'status' => join(PHP_EOL, $status),
-                'fileId' => $file->id
+                'position' => isset($position) ? join(PHP_EOL, $position) : 0,
+                'duration' => isset($duration) ? join(PHP_EOL, $duration) : 0,
+                'volume' => isset($volume) ? join(PHP_EOL, $volume) : 100,
+                'source' => isset($source) ? join(PHP_EOL, $source) : null,
+                'status' => isset($status) ? join(PHP_EOL, $status) : null,
+                'fileId' => (isset($file->id) ? $file->id : null)
             ]
         ]);
     }
