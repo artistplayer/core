3ceb8567774f63d3200305be2e437420851d2516
Maikel ten Voorde <info@matevo.nl>
2019-04-01 16:55:12 +0200

    TTC

--- setup/project.sh
+++ setup/project.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
+# Create database, if not exists
+if [ ! -f /home/signalize/database.sqlite ]; then
+    sudo su - signalize -c "touch /home/signalize/database.sqlite"
+    sudo chmod 0777 /home/signalize/database.sqlite
+fi
+
 # Unpack project
 sudo apt-get install git -y
-sudo su - signalize -c "composer clearcache"
 
 if [ ! -f /home/signalize/core/composer.lock ]; then
     sudo su - signalize -c "cd /home/signalize && git clone https://github.com/artistplayer/core.git core"
     sudo su - signalize -c "cd core && composer install"
 else
+    sudo su - signalize -c "composer clearcache"
     sudo su - signalize -c "cd core && composer update"
 fi
 
 # Config project
 sudo su - signalize -c "cd core && cp .env.example .env"
 
-# Create database, if not exists
-if [ ! -f /home/signalize/database.sqlite ]; then
-    sudo su - signalize -c "touch /home/signalize/database.sqlite"
-    sudo chmod 0777 /home/signalize/database.sqlite
-fi
 
 sudo chmod 0777 core/storage -Rf
 sudo su - signalize -c "cd core && php artisan migrate"
