de6f6578ff1845152e8be5711f098e7b1d20afdf
Maikel ten Voorde <info@matevo.nl>
2019-04-01 17:37:23 +0200

    TTC

--- setup/bluetooth.sh
+++ setup/bluetooth.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt-get install python-dbus rng-tools hostapd -y
+
+# Install OMX Service
+sudo ln -s /home/signalize/core/services/bluetooth.service /etc/systemd/system/signalize-bluetooth.service
+sudo systemctl start signalize-bluetooth
+sudo systemctl enable signalize-bluetooth
