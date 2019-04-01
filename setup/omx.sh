407d39cc3187c54243d0ca20737d499a66469a6f
Maikel ten Voorde <info@matevo.nl>
2019-04-01 17:39:32 +0200

    TTC

--- setup/omx.sh
+++ setup/omx.sh
@@ -3,6 +3,7 @@
 sudo apt-get install omxplayer python-dbus -y
 
 # Install OMX Service
+sudo chmod 0777 /home/signalize/core/services/omx.service
 sudo ln -s /home/signalize/core/services/omx.service /etc/systemd/system/signalize-omx.service
 sudo systemctl start signalize-omx
 sudo systemctl enable signalize-omx
--- setup/project.sh
+++ setup/project.sh
@@ -33,6 +33,7 @@ sudo ln -s /home/signalize/core/signalize.conf /etc/nginx/sites-enabled/default
 sudo systemctl reload nginx
 
 # Setup Socket Service
+sudo chmod 0777 /home/signalize/core/services/socket.service
 sudo ln -s /home/signalize/core/services/socket.service /etc/systemd/system/signalize-socket.service
 sudo systemctl start signalize-socket
 sudo systemctl enable signalize-socket
