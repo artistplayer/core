d71d35d859d6beab6b38c8bf2c42461a27068ac2
Maikel ten Voorde <info@matevo.nl>
2019-04-01 16:43:17 +0200

    TTC

--- setup/composer.sh
+++ setup/composer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -sS https://getcomposer.org/installer | php &
+sudo sh -c "curl -sS https://getcomposer.org/installer | php"
 sudo mv composer.phar /usr/bin/composer
 
 echo "Verify Composer Install"
