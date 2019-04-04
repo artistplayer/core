98da8af9114fdfc177f234731e52d8b67378e8ce
Maikel ten Voorde <info@matevo.nl>
2019-04-04 13:03:35 +0200

    TTC

--- setup/hotspot.sh
+++ setup/hotspot.sh
@@ -8,9 +8,11 @@ source-directory /etc/network/interfaces.d
 
 auto lo
 iface lo inet loopback
-iface eth0 inet manual
+
+auto eth0
+iface eth0 inet dhcp
+
 allow-hotplug wlan0
-auto wlan0
 iface wlan0 inet static
 address 192.168.1.1
 netmask 255.255.255.0
