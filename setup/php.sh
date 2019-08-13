3c24b6b38eaddf1806c614c747a57a0e80247881
artistplayer <48606073+artistplayer@users.noreply.github.com>
2019-08-13 16:04:52 +0200

    Change Stretch to Buster

--- setup/php.sh
+++ setup/php.sh
@@ -3,7 +3,7 @@
 # Update package manager
 sudo apt-get install ca-certificates apt-transport-https -y
 sudo wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
-sudo echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php7.list
+sudo echo "deb https://packages.sury.org/php/ buster main" | sudo tee /etc/apt/sources.list.d/php7.list
 sudo apt-get update -y
 
 # Install PHP and Required Libraries
