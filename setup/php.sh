2768d3e37c664b2becdf7d7b19738ffc5fae8e17
artistplayer <48606073+artistplayer@users.noreply.github.com>
2019-10-27 20:10:39 +0100

    Update php.sh

--- setup/php.sh
+++ setup/php.sh
@@ -8,3 +8,4 @@ sudo apt-get update -y
 
 # Install PHP and Required Libraries
 sudo apt-get install php7.3-fpm php7.3-opcache php7.3-curl php7.3-mbstring php7.3-pgsql php7.3-zip php7.3-xml php7.3-gd php7.3-sqlite3 -y
+sudo sed -i 's/memory_limit = -1/memory_limit = 512MB/g'  /etc/php/7.3/cli/php.ini
