3c8e524ddf94d6c1b3b2a6c9965f7224468c35b5
artistplayer <48606073+artistplayer@users.noreply.github.com>
2019-11-14 15:48:07 +0100

    Update hifiberry.sh

--- setup/hifiberry.sh
+++ setup/hifiberry.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 if (whiptail --title "Install HifiBerry DAC Driver" --yesno "**** This will enable the hifiberry audio driver and disable the system audio driver ****" 12 78)
 then setupHifiBerry; else echo "SKIP"; fi
 
