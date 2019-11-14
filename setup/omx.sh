b6e397c1a4547c87265b40b03d015e5ec43c105c
artistplayer <48606073+artistplayer@users.noreply.github.com>
2019-11-14 22:50:59 +0100

    Update omx.sh

--- setup/omx.sh
+++ setup/omx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get install omxplayer python-dbus -y
+sudo apt-get install mplayer -y
 
-# Install OMX Service
-sudo chmod 0777 /home/signalize/core/services/signalize-omx.service
-sudo systemctl enable /home/signalize/core/services/signalize-omx.service
+# Install MPlayer Service
+sudo chmod 0777 /home/signalize/core/services/signalize-mplayer.service
+sudo systemctl enable /home/signalize/core/services/signalize-mplayer.service
